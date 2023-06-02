48ad4f4381a86a5341a70ad049fe62bab21f6729
Byeongjun1022 <kbj2738@kaist.ac.kr>
2023-06-02 13:31:18 +0900

    npy to jpg for depth map

--- npy_2_jpg.py
+++ npy_2_jpg.py
@@ -3,6 +3,8 @@ import argparse
 import numpy as np
 import PIL.Image as pil
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.cm as cm
 
 _DEPTH_COLORMAP = plt.get_cmap('plasma', 256)  # for plotting
 def parse_args():
@@ -19,17 +21,36 @@ def parse_args():
 
     return parser.parse_args()
 
-
 def npy_to_jpg(args):
-    if not os.path.exists(os.path.join(args.output_pth, args.model_type)):
+    model_name = args.npy_pth.split('/')[-4]
+    if not os.path.exists(os.path.join(args.output_pth, model_name)):
         print('Since output path does not exist, make output folder')
-        os.makedirs(os.path.join(args.output_pth, args.model_type))
+        os.makedirs(os.path.join(args.output_pth, model_name))
+        # os.makedirs(os.path.join(args.output_pth, args.model_type))
 
     npy_list = np.load(args.npy_pth)
     for i in range(len(npy_list)):
         depth_color=_DEPTH_COLORMAP(npy_list[i])[..., :3]
         img=pil.fromarray((depth_color*255).astype(np.uint8))
-        img.save(os.path.join(args.output_pth, args.model_type, f'disp_{i}.jpg'))
+        img.save(os.path.join(args.output_pth, model_name, f'disp_{i}.jpg'))
+
+def npy_to_jpg_monodepth_2(args):
+    model_name = args.npy_pth.split('/')[-4]+'_2'
+    if not os.path.exists(os.path.join(args.output_pth, model_name)):
+        print('Since output path does not exist, make output folder')
+        os.makedirs(os.path.join(args.output_pth, model_name))
+        # os.makedirs(os.path.join(args.output_pth, args.model_type))
+
+    npy_list = np.load(args.npy_pth)
+    for i in range(len(npy_list)):
+        # disp_resized_np = disp_resized.squeeze().cpu().numpy()
+        disp_resized_np = npy_list[i]
+        vmax = np.percentile(disp_resized_np, 95)
+        normalizer = mpl.colors.Normalize(vmin=disp_resized_np.min(), vmax=vmax)
+        mapper = cm.ScalarMappable(norm=normalizer, cmap='magma')
+        colormapped_im = (mapper.to_rgba(disp_resized_np)[:, :, :3] * 255).astype(np.uint8)
+        im = pil.fromarray(colormapped_im)
+        im.save(os.path.join(args.output_pth, model_name, f'disp_{i}.jpg'))
 
 def gt_to_jpg(args):
     npy_list=np.load(args.gt_pth, fix_imports=True, encoding='latin1',allow_pickle=True)['data']
@@ -40,5 +61,6 @@ def gt_to_jpg(args):
 
 if __name__ == '__main__':
     args=parse_args()
+    npy_to_jpg_monodepth_2(args)
     # npy_to_jpg(args)
-    gt_to_jpg(args)
+    # gt_to_jpg(args)
---
+++ npy_2_jpg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python npy_2_jpg.py --npy_pth ~/tmp/lite_mab_no_residual_3/models/weights_25/disps_eigen_split.npy --output_pth /mnt_2
+#python npy_2_jpg.py --npy_pth ~/tmp/lite_lgfi_se_2023-04-04\ 23:29:58/models/weights_28/disps_eigen_split.npy --output_pth /mnt_2

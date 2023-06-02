dea3ffbcdedeaac6e3d9f1c199cba3cca19e6a49
Byeongjun1022 <kbj2738@kaist.ac.kr>
2023-06-02 15:14:41 +0900

    Update eval_mult.sh

--- eval_mult.sh
+++ eval_mult.sh
@@ -5,11 +5,13 @@
 #  python evaluate_depth.py --load_weights_folder  ~/tmp/mono_model_panoptic/models/weights_$var --eval_mono --panoptic_decoder
 #done
 
-for ((var=15 ; var < 30 ; var++));
+for ((var=15; var < 20 ; var++));
 do
-  # CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder  ~/tmp/lite_rm_dilation_8_2023-04-28\ 15:25:08/models/weights_$var --eval_mono --lite  --global_block_type MAB --gpu_num 0
-#  CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_mab_no_residual_2023-05-27\ 23:03:39/models/weights_$var --eval_mono --lite  --global_block_type MAB --gpu_num 0
-#  CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_mab_no_residual_2023-05-28\ 12:31:11/models/weights_$var --eval_mono --lite  --global_block_type MAB --gpu_num 0
-#  CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_maxim_2x_2023-05-31\ 01:55:07/models/weights_$var --eval_mono --lite --maxim --global_block_type MAB --gpu_num 0
-  CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_mab_se_2x_2023-06-01\ 16:20:38/models/weights_$var --eval_mono --lite --global_block_type MAB --gpu_num 0 --mab_se
+#  python evaluate_depth.py --load_weights_folder  ~/tmp/lite_lgfi_se_nores_2023-04-05\ 19:56:19/models/weights_$var --eval_mono --lite
+#  python evaluate_depth.py --load_weights_folder  ~/tmp/lite_parallel_2023-04-10\ 23:29:50/models/weights_$var --eval_mono --lite --parallel
+#  python evaluate_depth.py --load_weights_folder  ~/tmp/lite_lgfi_se_2023-04-04\ 23:29:58/models/weights_28 --eval_mono --lite --global_block_type LGFI_SE --res --save_pred_disps
+ python evaluate_depth.py --load_weights_folder  ~/tmp/lite_4/models/weights_$var --eval_mono --lite --global_block_type LGFI --save_pred_disps
+#  python evaluate_depth.py --load_weights_folder  ~/tmp/lite_mab_no_residual_3/models/weights_$var --eval_mono --lite --global_block_type MAB
+  # python evaluate_depth.py --load_weights_folder  ~/tmp/lite_mab_no_residual_2023-05-31\ 01:34:53/models/weights_$var --eval_mono --lite --global_block_type MAB
+#  python evaluate_depth.py --load_weights_folder  ~/tmp/lite_mab_no_residual_2023-05-29\ 00:23:23/models/weights_$var --eval_mono --lite --global_block_type MAB
 done

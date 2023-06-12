7f6bf296e7bb07bfe28a7f9d6b3b6a08da96ce4f
Byeongjun1022 <kbj2738@kaist.ac.kr>
2023-06-12 15:42:59 +0900

    fully use Edge_vit without CDC blocks added

--- eval_mult.sh
+++ eval_mult.sh
@@ -5,7 +5,7 @@
 #  python evaluate_depth.py --load_weights_folder  ~/tmp/mono_model_panoptic/models/weights_$var --eval_mono --panoptic_decoder
 #done
 
-for ((var=21 ; var < 22 ; var++));
+for ((var=16 ; var < 30 ; var++));
 do
   # CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder  ~/tmp/lite_rm_dilation_8_2023-04-28\ 15:25:08/models/weights_$var --eval_mono --lite  --global_block_type MAB --gpu_num 0
 #  CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_mab_no_residual_2023-05-27\ 23:03:39/models/weights_$var --eval_mono --lite  --global_block_type MAB --gpu_num 0
@@ -15,6 +15,7 @@ do
 # CUDA_VISIBLE_DEVICES=1 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_max_se_2023-06-05\ 14:27:26/models/weights_$var --eval_mono --lite --global_block_type Max --gpu_num 0 --mab_se #--save_pred_disps
 #   CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_mab_adamw_2023-06-07\ 10:42:31/models/weights_$var --eval_mono --lite --global_block_type MAB --gpu_num 0 --mab_se --save_pred_disps
   # CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_4/models/weights_$var --eval_mono --lite --global_block_type LGFI --gpu_num 0 #--save_pred_disps
-   CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_max_wnhead_w_2023-06-0813:15:18/models/weights_$var --eval_mono --lite --global_block_type Max --gpu_num 0 --save_pred_disps
+#   CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_max_wnhead_w_2023-06-0813:15:18/models/weights_$var --eval_mono --lite --global_block_type Max --gpu_num 0 --save_pred_disps
+   CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py --load_weights_folder /mnt_2/tmp/lite_edge_2023-06-1123:07:09/models/weights_$var --eval_mono --lite --global_block_type Edge --gpu_num 0 #--save_pred_disps
 done
 
